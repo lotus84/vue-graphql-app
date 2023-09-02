225b711ba9bb57261a0e3c5aa3b1cafb8d9d1215
Elena Mikirtumova <mikirtumova84@gmail.com>
2023-09-02 17:05:22 +0400

    add tailwind css

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,32 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
+  env: {
+    node: true,
+  },
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
+  rules: {
+    'vue/array-bracket-spacing': 'error',
+    'vue/arrow-spacing': 'error',
+    'vue/block-spacing': 'error',
+    'vue/brace-style': 'error',
+    'vue/camelcase': 'error',
+    'vue/comma-dangle': ['error', 'always-multiline'],
+    'vue/component-name-in-template-casing': 'error',
+    'vue/dot-location': ['error', 'property'],
+    'vue/key-spacing': 'error',
+    'vue/keyword-spacing': 'error',
+    'vue/no-boolean-default': ['error', 'default-false'],
+    'vue/no-deprecated-scope-attribute': 'error',
+    'vue/no-empty-pattern': 'error',
+    'vue/object-curly-spacing': ['error', 'always'],
+    'vue/padding-line-between-blocks': 'error',
+  },
   parserOptions: {
     ecmaVersion: 'latest'
   }
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ["Manrope", "sans-serif"],
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      primary: "#2A254B",
+      secondary: "#4E4D93",
+      grey: "#F9F9F9",
+      lavender: "#EBE8F4",
+      white: "#FFFFFF",
+      melrose: "#CAC6DA",
+      tolopea: "#1E193E",
+      jam: "#DB574D",
+      blue: "#0ea5e9",
+      pearl: "#A29EAE",
+      malachite: "#22C55E",
+    },
+  },
+  plugins: [],
+  darkMode: "class",
+}
+
