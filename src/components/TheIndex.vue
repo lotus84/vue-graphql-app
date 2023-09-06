2627ba060a92f0e9d00848fea7015927fee948b6
Elena Mikirtumova <mikirtumova84@gmail.com>
2023-09-06 23:30:38 +0400

    add character card

--- src/components/TheIndex.vue
+++ src/components/TheIndex.vue
@@ -1,32 +1,11 @@
 <script setup lang="ts">
-import gql from 'graphql-tag';
-import { useQuery } from '@vue/apollo-composable';
-
 import TheIntro from '@/components/TheIntro.vue';
 import StatsInfo from '@/components/StatsInfo.vue';
-
-const { result } = useQuery(gql`
-query {
-  characters(page: 2, filter: { name: "Morty" }) {
-    info {
-      count
-    }
-    results {
-      name
-    }
-  }
-  location(id: 1) {
-    id
-  }
-  episodesByIds(ids: [1, 2]) {
-    id
-  }
-}
-`)
+import CharactersList from '@/components/CharactersList.vue';
 </script>
 
 <template>
   <TheIntro />
+  <CharactersList />
   <StatsInfo />
-  <div>{{ result }}</div>
 </template>
